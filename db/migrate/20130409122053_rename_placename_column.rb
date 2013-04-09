20dc1ad1d704e2eb4a56b5670a2b124ed8323eb5
Elrassam <eng.elrassam@hotmail.com>
2013-04-09 23:32:27 +0200

    user_v6

--- db/migrate/20130409122053_rename_placename_column.rb
+++
@@ -1,5 +0,0 @@
-class RenamePlacenameColumn < ActiveRecord::Migration
-  def change
-  	rename_column(:places, :p_name, :placename)
-  end
-end
--- db/migrate/20130409180008_renamereservation.rb
+++
@@ -1,6 +0,0 @@
-class Renamereservation < ActiveRecord::Migration
-  def change
-  	rename_column(:reservations, :from, :datefrom)
-  	rename_column(:reservations, :to, :dateto)
-  end
-end
